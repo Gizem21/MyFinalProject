2b1fcc4863c1c000c7e9e33a24cab929b9eae75b
Gizem21 <77542061+Gizem21@users.noreply.github.com>
2021-01-31 00:33:44 +0300

    Add project files.

---
+++ Business/Concreate/ProductManager.cs
@@ -0,0 +1,30 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concreate;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concreate
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            //İş kolları
+            //Yetkisi var mı ?
+
+            return _productDal.GetAll();
+           
+        }
+
+        
+    }
+}
